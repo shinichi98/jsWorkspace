47c8febfa7f5cf4db24530e05c19c9deba30d9ba
shinichi <iamshinichikira@gmail.com>
2023-04-02 13:34:50 +0530

    spread commit

--- node/restSpread.js
+++ node/restSpread.js
@@ -1,4 +1,12 @@
-function myfunc(first,...remaining){
-    return [first,remaining];
-}
-console.log(myfunc('1','2','3'))
+// function myfunc(first,...remaining){
+//     return [first,remaining];
+// }
+// console.log(myfunc('1','2','3'))
+
+let a=['a','b']
+let b=['a','b']
+var c,d,e;
+c=a+b;
+d=[...a,...b];
+console.log(c)
+console.log(d)
